72da116f19494402d4406e4b9fec8afd35ada709
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2024-01-12 00:50:17 +0000

    feature: implements extension classes for adding repo to the service collection

---
+++ source/EzRepositories/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,70 @@
+﻿using EzRepositories.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzRepositories.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddEzRepository<TEntity>(
+            this IServiceCollection services,
+            ServiceLifetime repositoryLifetime = ServiceLifetime.Scoped)
+            where TEntity : class, IEntity
+        {
+            services.Add(
+                    new ServiceDescriptor(
+                            typeof(IRepository<TEntity>),
+                            typeof(Repository<TEntity>),
+                            repositoryLifetime
+                        )
+                );
+
+            return services;
+        }
+
+        public static IServiceCollection AddEzRepository<TEntity, TDbContext>(
+             this IServiceCollection services,
+             ServiceLifetime repositoryLifetime = ServiceLifetime.Scoped
+            )
+            where TEntity : class, IEntity
+            where TDbContext : DbContext
+        {
+            services.Add(
+                    new ServiceDescriptor(
+                        typeof(IRepository<TEntity>),
+                        serviceProvider =>
+                        {
+                            var dbcontext = serviceProvider.GetRequiredService<TDbContext>();
+                            return ActivatorUtilities.CreateInstance<Repository<TEntity>>(serviceProvider, [dbcontext]);
+                        }, repositoryLifetime
+                    )
+                );
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomEzRepository<TEntity, TCustomRepository>(
+             this IServiceCollection services,
+             ServiceLifetime repositoryLifetime = ServiceLifetime.Scoped
+            )
+            where TEntity : class, IEntity
+            where TCustomRepository : IRepository<TEntity>
+        {
+            services.Add(
+                new ServiceDescriptor(
+                    typeof(IRepository<TEntity>),
+                    typeof(TCustomRepository),
+                    repositoryLifetime
+                )
+             );
+
+            return services;
+        }
+    }
+}
